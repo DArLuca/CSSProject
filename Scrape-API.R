b4922799ed6b281f8e9f21c6a4a637598c00302f
julesop <julia@beck-family.ch>
2024-12-10 11:19:27 +0100

    test file ignore

--- Scrape-API.R
+++ Scrape-API.R
@@ -1,6 +1,16 @@
+library(httr)
+library(rvest)
+library(jsonlite)
+library(dplyr)
+
 ### get overview of scrapable newspapers or APIs
 
+
 ### newspapers of interest
+https://www.heritage.org/press/press-archive
 
 
 ### Scraping 
+
+
+nyt_api <- "https://api.nytimes.com/svc/archive/v1/2024/1.json?api-key=MrH81Lr74JQGARzZrQ2pRgb1vC9wr7kl"
